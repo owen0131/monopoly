acce9926c909d46a4aca3bb77b2e6f8705b61855
Jake <jarveson@gmail.com>
2017-10-31 15:20:17 -0500

    Initial project with Setup The Game Steps

---
+++ MonopolyGame/TestProj/SetupGameFeature.feature
@@ -0,0 +1,107 @@
+﻿Feature: Setup the game
+
+Scenario Outline: Start the app
+	When I start the game
+	And Player is prompted for how many players
+	And They enter <Players>
+	Then User receieves <Response>
+	
+	Examples: 
+	| Players | Response |
+	| 1       | Fail     |
+	| 2       | Success  |
+	| 6       | Success  |
+	| 7       | Fail     |
+	| 0       | Fail     |
+
+Scenario: Give Player money 
+	When Player is created 
+	Then Their balance is $1500
+
+Scenario: Verify Buildings
+	When All players are created and they receive money
+	Then The bank contains 32 houses and 12 hotels
+
+Scenario: Assign Order
+	When Number of players is selected
+	Then Order is randomly assigned
+
+Scenario Outline: Display Board
+	When the board is displayed
+	Then <Location> is <Name>
+
+	Examples:
+	| Location | Name                    |
+	| 1        | "GO"                    |
+	| 2        | "Mediterranean Avenue"  |
+	| 3        | "Community Chest"       |
+	| 4        | "Baltic Avenue"         |
+	| 5        | "Income Tax"            |
+	| 6        | "Reading Railroad"      |
+	| 7        | "Oriental Avenue"       |
+	| 8        | "Chance"                |
+	| 9        | "Vermont Avenue"        |
+	| 10       | "Conneticut Avenue"     |
+	| 11       | "Jail"                  |
+	| 12       | "St. Charles Place"     |
+	| 13       | "Electric Company"      |
+	| 14       | "States Avenue"         |
+	| 15       | "Virginia Avenue"       |
+	| 16       | "Pennsyvania Railroad"  |
+	| 17       | "St. James Place"       |
+	| 18       | "Community Chest"       |
+	| 19       | "Tennessee Avenue"      |
+	| 20       | "New York Avenue"       |
+	| 21       | "Free Parking"          |
+	| 22       | "Kentucky Avenue"       |
+	| 23       | "Chance"                |
+	| 24       | "Indiana Avenue"        |
+	| 25       | "Illinois Avenue"       |
+	| 26       | "B & O Railroad"        |
+	| 27       | "Atlantic Avenue"       |
+	| 28       | "Ventnor Avenue"        |
+	| 29       | "Water Works"           |
+	| 30       | "Marvin Gardens"        |
+	| 31       | "Go To Jail"            |
+	| 32       | "Pacific Avenue"        |
+	| 33       | "North Carolina Avenue" |
+	| 34       | "Community Chest"       |
+	| 35       | "Pennsylvania Avenue"   |
+	| 36       | "Short Line"            |
+	| 37       | "Chance"                |
+	| 38       | "Park Place"            |
+	| 39       | "Luxury Tax"            |
+	| 40       | "Boardwalk"             |
+
+Scenario Outline: Player Picks a Token
+	When Order is assigned
+	And Player is prompted to pick a token
+	And Player enters <Token>
+	Then User recieves <Event>
+
+	Examples:
+	| Token        | Event   |
+	| "Battleship" | Success |
+	| "Hat"        | Success |
+	| "T-Rex"      | Success |
+	| "Cat"        | Success |
+	| "Car"        | Success |
+	| "Duck"       | Success |
+	| "Penguin"    | Success |
+	| "Dog"        | Success |
+	| "NotDog"     | Fail    |
+	| ""           | Fail    |
+
+Scenario Outline: Player selects Token
+	When Player selects Token <Token>
+	Then Available Tokens left are <TokensLeft>
+
+	Examples:
+	| Token        | TokensLeft                                 |
+	| "Battleship" | "Hat, T-Rex, Cat, Car, Duck, Penguin, Dog" |
+
+
+Scenario: Place tokens on board
+	When the game is setup
+	Then the board is displayed
+	And All pieces are on "go"
---
+++ MonopolyGame/TestProj/SetupTheGameSteps.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace TestProj
+{
+    [Binding]
+    public class SetupTheGameSteps
+    {
+        [When(@"I start the game")]
+        public void WhenIStartTheGame()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"Player is prompted for how many players")]
+        public void WhenPlayerIsPromptedForHowManyPlayers()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"They enter (.*)")]
+        public void WhenTheyEnter(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"Player is created")]
+        public void WhenPlayerIsCreated()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"All players are created and they receive money")]
+        public void WhenAllPlayersAreCreatedAndTheyReceiveMoney()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"Number of players is selected")]
+        public void WhenNumberOfPlayersIsSelected()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"the board is displayed")]
+        public void WhenTheBoardIsDisplayed()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"Order is assigned")]
+        public void WhenOrderIsAssigned()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"Player is prompted to pick a token")]
+        public void WhenPlayerIsPromptedToPickAToken()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"Player enters ""(.*)""")]
+        public void WhenPlayerEnters(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"Player selects Token ""(.*)""")]
+        public void WhenPlayerSelectsToken(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"the game is setup")]
+        public void WhenTheGameIsSetup()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"User receieves Fail")]
+        public void ThenUserReceievesFail()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"Their balance is \$(.*)")]
+        public void ThenTheirBalanceIs(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"The bank contains (.*) houses and (.*) hotels")]
+        public void ThenTheBankContainsHousesAndHotels(int p0, int p1)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"Order is randomly assigned")]
+        public void ThenOrderIsRandomlyAssigned()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"(.*) is ""(.*)""")]
+        public void ThenIs(int p0, string p1)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"User recieves Success")]
+        public void ThenUserRecievesSuccess()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"Available Tokens left are ""(.*)""")]
+        public void ThenAvailableTokensLeftAre(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the board is displayed")]
+        public void ThenTheBoardIsDisplayed()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"All pieces are on ""(.*)""")]
+        public void ThenAllPiecesAreOn(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
